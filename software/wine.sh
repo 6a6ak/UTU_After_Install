6fc69cebd697043868e8a887a9193c692b5cea43
Babak Aharifard <babak@adbud.io>
2023-04-06 20:05:31 +0200

    wine added

---
+++ software/wine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+sudo apt update
+sudo apt install -y winehq-stable
