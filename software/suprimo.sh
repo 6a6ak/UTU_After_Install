7efe0cf6cee94f59f15e4fc39c3ed13cc3901085
Babak Aharifard <babak@adbud.io>
2023-04-06 23:04:40 +0200

    not working yet

--- software/suprimo.sh
+++ software/suprimo.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 cd ~/Downloads
-wget https://download.supremocontrol.com/download/SUPREMO.EXE
+wget https://www.nanosystems.it/public/download/Supremo.exe?_ga=2.142121586.1551105511.1680806505-1173119374.1680806505
