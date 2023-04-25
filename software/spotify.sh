ad56337439e98dec072eaa9ca1fd071228246b6c
0x134134K <babak@adbud.io>
2023-04-25 15:16:42 +0200

    Spotify

--- software/spotify.sh
+++ software/spotify.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Function to install Spotify
-install_spotify() {
-  echo "Installing Spotify..."
-  curl -sS https://download.spotify.com/debian/pubkey_4E6AF4E5.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/spotify.gpg > /dev/null
+# Function to install Spotify using Snap
+install_spotify_snap() {
+  echo "Installing Spotify using Snap..."
+  sudo snap install spotify
+  echo "Spotify has been installed."
+}
+
+# Function to install Spotify using Debian package
+install_spotify_debian() {
+  echo "Installing Spotify using Debian package..."
+  curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-  sudo apt-get update && sudo apt-get install -y spotify-client
+  sudo apt-get update && sudo apt-get install spotify-client
   echo "Spotify has been installed."
 }
 
@@ -29,17 +36,19 @@ uninstall_spotify() {
 # Main loop
 while true; do
   echo "Select an option:"
-  echo "1. Install Spotify"
-  echo "2. Remove Spotify ads"
-  echo "3. Uninstall Spotify"
+  echo "1. Install Spotify using Snap"
+  echo "2. Install Spotify using Debian package"
+  echo "3. Remove Spotify ads"
+  echo "4. Uninstall Spotify"
   echo "0. Exit"
-  read -p "Enter your choice (0-3): " choice
+  read -p "Enter your choice (0-4): " choice
 
   case $choice in
-    1) install_spotify ;;
-    2) remove_ads ;;
-    3) uninstall_spotify ;;
+    1) install_spotify_snap ;;
+    2) install_spotify_debian ;;
+    3) remove_ads ;;
+    4) uninstall_spotify ;;
     0) echo "Exiting..."; exit 0 ;;
-    *) echo "Invalid option. Please enter a number between 0 and 3." ;;
+    *) echo "Invalid option. Please enter a number between 0 and 4." ;;
   esac
 done
