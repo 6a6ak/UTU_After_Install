0a808c4ed048b941ba1d6dd03bd6e78792865969
0x134134K <babak@adbud.io>
2023-04-25 13:51:46 +0200

    older

--- software/PhpStorm.sh
+++ software/PhpStorm.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
 function install_phpstorm {
-    # Download PHPStorm
-    wget -O /tmp/phpstorm.tar.gz https://download.jetbrains.com/webide/PhpStorm-2021.1.tar.gz
-
-    # Extract the archive to /opt directory
-    sudo tar -xzf /tmp/phpstorm.tar.gz -C /opt/
-
-    # Create a symlink for the phpstorm executable
-    sudo ln -s /opt/PhpStorm-*/bin/phpstorm.sh /usr/local/bin/phpstorm
-
-    # Clean up the downloaded archive
-    rm /tmp/phpstorm.tar.gz
+    # Install PHPStorm via snap
+    sudo snap install phpstorm --classic
 
     echo "PHPStorm installed successfully!"
 }
 
 function uninstall_phpstorm {
-    # Remove the symlink
-    sudo rm /usr/local/bin/phpstorm
-
-    # Remove the PHPStorm directory
-    sudo rm -rf /opt/PhpStorm-*/
+    # Remove PHPStorm via snap
+    sudo snap remove phpstorm
 
     echo "PHPStorm uninstalled successfully!"
 }
 
 while true; do
     echo "Enter 1 to install PHPStorm"
-    echo "Enter 2 to uninstall and purge PHPStorm (removes configuration files)"
+    echo "Enter 2 to uninstall PHPStorm"
     echo "Enter 0 to exit"
 
     read -p "Enter your choice: " choice
@@ -39,9 +27,6 @@ while true; do
             ;;
         2)
             uninstall_phpstorm
-            # Remove configuration files
-            sudo rm -rf ~/.config/JetBrains/PhpStorm*
-            echo "PHPStorm purged successfully!"
             ;;
         0)
             exit 0
